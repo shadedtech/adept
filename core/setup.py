2eb6f598df8cdae1124ec47e5e9d5087399c2a05
Joe Tatusko <jtatusko@gmail.com>
2023-02-04 18:46:13 -0500

    setup.py, logging null handler

---
+++ core/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='adept',
+    version='0,0.0dev0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='BitLords',
+    author_email='',
+    description=''
+)
