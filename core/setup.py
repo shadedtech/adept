a2a765334b3cd0c4de8212997467c060f80ff7a6
Joe Tatusko <jtatusko@gmail.com>
2023-02-04 18:48:03 -0500

    gitignore idea, logging, setup.py packages

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='adept',
     version='0,0.0dev0',
-    packages=[''],
+    packages=["adept", "adept.run", "adept.util"],
     url='',
     license='MIT',
     author='BitLords',
