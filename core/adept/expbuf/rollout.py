d19a0e15be7ce79f327b34388ff2a6b535a3fd7d
Joe Tatusko <jtatusko@gmail.com>
2023-02-20 19:46:32 -0500

    Make Rollout more general

--- core/adept/expbuf/rollout.py
+++ core/adept/expbuf/rollout.py
@@ -16,7 +16,9 @@ if typing.TYPE_CHECKING:
 class Rollout(ExpBuf):
     def __init__(self, rollout_len: int = 20):
         self._rollout_len = rollout_len
-        self._buffer: dict[str, Tensor] = {}
+        self._buffer: dict[
+            str, Tensor | typing.Iterable[Tensor] | dict[str, Tensor]
+        ] = {}
         self._cur_ix = 0
 
     def step(self, actor_entry: Experience, env_entry: Experience) -> bool:
@@ -24,20 +26,13 @@ class Rollout(ExpBuf):
         if self.ready():
             self.reset()
         # Lazily initialize the buffer
-        for key, tensor in {**actor_entry, **env_entry}.keys():
-            if key not in self._buffer:
-                self._buffer[key] = torch.empty(
-                    self._rollout_len,
-                    *tensor.shape,
-                    device=tensor.device,
-                    dtype=tensor.dtype
-                )
-            self._buffer[key][self._cur_ix] = actor_entry[key]
+        for rollout_buf, item in self._zip_with_buffer({**actor_entry, **env_entry}):
+            rollout_buf[self._cur_ix] = item
         self._cur_ix += 1
         return self.ready()
 
     def reset(self) -> bool:
-        for tensor in self._buffer.values():
+        for tensor in self._iter_buffer():
             tensor.detach_()
         self._cur_ix = 0
         return self.ready()
@@ -52,6 +47,47 @@ class Rollout(ExpBuf):
     def __len__(self):
         return self._rollout_len
 
+    def _zip_with_buffer(self, experience: Experience) -> typing.Iterable[tuple[Tensor, Tensor]]:
+        """Yields a tuple of (rollout buffer, item)
+
+        The rollout buffer is lazily initialized to the correct shape and dtype.
+        """
+        def make_empty(t: Tensor) -> Tensor:
+            return torch.empty(
+                self._rollout_len, *t.shape, device=t.device, dtype=t.dtype
+            )
+
+        for key, item in experience.items():
+            if isinstance(item, Tensor):
+                if key not in self._buffer:
+                    self._buffer[key] = make_empty(item)
+                yield self._buffer[key], item
+            elif isinstance(item, dict):
+                if key not in self._buffer:
+                    self._buffer[key] = {}
+                for k, v in item.items():
+                    if k not in self._buffer[key]:
+                        self._buffer[key][k] = make_empty(v)
+                    yield self._buffer[key][k], v
+            elif isinstance(item, typing.List):
+                if key not in self._buffer:
+                    self._buffer[key] = []
+                for i in range(len(item)):
+                    if i >= len(self._buffer[key]):
+                        self._buffer[key].append(make_empty(item[i]))
+                    yield self._buffer[key][i], item[i]
+
+    def _iter_buffer(self) -> typing.Iterator[Tensor]:
+        for item in self._buffer.values():
+            if isinstance(item, Tensor):
+                yield item
+            elif isinstance(item, dict):
+                for v in item.values():
+                    yield v
+            elif isinstance(item, typing.Iterable):
+                for v in item:
+                    yield v
+
 
 class OffPolicyRollout(Rollout):
     @configurable
@@ -62,8 +98,8 @@ class OffPolicyRollout(Rollout):
     def step(self, actor_entry: Experience, env_entry: Experience) -> bool:
         result = super().step(actor_entry, env_entry)
         if not self.is_initialized:
-            for tensor in self._buffer.values():
-                tensor.share_memory_()
+            for t in self._iter_buffer():
+                t.share_memory_()
             self.is_initialized = True
         return result
 
